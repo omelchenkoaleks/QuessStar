478d7d7792f12418bbfa376057ce824dd001d2d5
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-07-01 09:48:55 +0300

    два класса AsyncTask - для загрузки контента и загрузки изображения ...

--- app/src/main/java/com/omelchenkoaleks/quessstar/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/quessstar/MainActivity.java
@@ -1,14 +1,112 @@
 package com.omelchenkoaleks.quessstar;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+    private String mUrlConnection = "http://www.posh24.se/kandisar";
+
+    private Button mButton_0;
+    private Button mButton_1;
+    private Button mButton_2;
+    private Button mButton_3;
+
+    private ImageView mSrarImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButton_0 = findViewById(R.id.button_0);
+        mButton_1 = findViewById(R.id.button_1);
+        mButton_2 = findViewById(R.id.button_2);
+        mButton_3 = findViewById(R.id.button_3);
+
+        mSrarImageView = findViewById(R.id.star_image_view);
+
+        getContent();
+    }
+
+    private void getContent() {
+        DownloadContentTask contentTask = new DownloadContentTask();
+        try {
+            String content = contentTask.execute(mUrlConnection).get();
+            Log.i("urlContent", content);
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static class DownloadContentTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder result = new StringBuilder();
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                BufferedReader bufferedReader = new BufferedReader(reader);
+                String line = bufferedReader.readLine();
+                while (line != null) {
+                    result.append(line);
+                    line = bufferedReader.readLine();
+                }
+                return result.toString();
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+
+            return null;
+        }
+    }
+
+    private static class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            URL url = null;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(in);
+                return bitmap;
+            } catch (java.io.IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+
+            return null;
+        }
     }
 }
